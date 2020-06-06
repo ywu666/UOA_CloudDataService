6a6440ced88bbf0905e1c206f878d0950f6d9af2
Emily Yang <zyan225@aucklanduni.ac.nz>
2020-06-07 11:12:42 +1200

    change sidebar button color when user clicks it
    Issue:
    User clicks the button in the side bar, only the directory changes, side bar has no indication on which button user just clicked.
    
    Solution:
    Change the background color of the button user just clicked.

--- javascript/SideBar.js
+++ javascript/SideBar.js
@@ -23,3 +23,18 @@ function ToggleExpandContact( buttonId, expandContactId ) {
 		flexContainer.style.paddingLeft = "20px";
 	}
 }
+
+function changeSideBarButtonColor( buttonId ) {
+	// change the background color of this button
+	const buttons = document.getElementsByClassName( "side-bar-button" );
+	for( let i = 0; i < buttons.length; i++ ) {
+		// change background color of all buttons to the default one
+		buttons[i].style.backgroundColor = "#b3d9ff";
+	}
+
+	// update the background color of the button clicked
+	console.log( buttonId )
+	console.log(document.getElementById( buttonId ).style.backgroundColor)
+	document.getElementById( buttonId ).style.backgroundColor = "#87CEEB";
+	console.log(document.getElementById( buttonId ).style.backgroundColor)
+}
